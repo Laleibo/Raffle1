da24adc39beeda74bd7a8dc530db7beb65cf2267
Laleibo <Laleibo@gmail.com>
2016-07-12 19:09:34 -0400

    buttons 7pm

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,5 +1,5 @@
 class Raffle < ApplicationRecord
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   has_many :users, through: :tickets
 
   belongs_to :winner, class_name: "User", foreign_key: "user_id", required: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   has_many :raffles, through: :tickets
   has_many :winner_raffles, class_name: "Raffle"
 end
